615f98402775b7437a140c3a1ff2c8f373c64cb0
Andrew <abujaki21@gmail.com>
2015-08-11 22:09:35 -0500

    Submit for assistance on line 44 of PortalMod.java

--- src/main/java/io/github/derpsquad/portalmod/PortalMod.java
+++ src/main/java/io/github/derpsquad/portalmod/PortalMod.java
@@ -1,18 +1,22 @@
 package io.github.derpsquad.portalmod;
 
-import io.github.derpsquad.portalmod.block.PortalModBlocks;
-import io.github.derpsquad.portalmod.item.PortalModItems;
+import io.github.derpsquad.portalmod.block.*;
+import io.github.derpsquad.portalmod.item.*;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = PortalMod.MODID, version = PortalMod.VERSION)
 public class PortalMod
@@ -33,5 +37,11 @@ public class PortalMod
 		logger.info("Hello World!");
 		PortalModBlocks.init();
 		PortalModItems.init();
+		
+		if(event.getSide() == Side.CLIENT)
+		{
+			RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
+			renderItem.getItemModelMesher().register(ItemTeleportationCore, new ModelResourceLocation(Reference.MODID + ":" + ((PortalMod) ItemTeleportationCore).getName(), "inventory") );
+		}
     }
 }
