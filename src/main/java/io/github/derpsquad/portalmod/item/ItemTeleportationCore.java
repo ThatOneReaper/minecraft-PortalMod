43f7c0ddde4fd597d25f07e712892a7ed71280b1
Richard Yin <richard.hongyi.yin@gmail.com>
2015-08-12 21:28:01 -0400

    Principle of least privilege is important

--- src/main/java/io/github/derpsquad/portalmod/item/ItemTeleportationCore.java
+++ src/main/java/io/github/derpsquad/portalmod/item/ItemTeleportationCore.java
@@ -1,12 +1,11 @@
 package io.github.derpsquad.portalmod.item;
 
-import net.minecraft.item.Item;
 
 public class ItemTeleportationCore extends ModItem {
 	
 	private static final String name = "teleportationCore";
 	
-	public ItemTeleportationCore() {
+	protected ItemTeleportationCore() {
 		super(name);
 	}
 	
