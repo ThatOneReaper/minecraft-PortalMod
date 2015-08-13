87bdbb6a4c6bccf975af36e1d912661feb7191b3
Richard Yin <richard.hongyi.yin@gmail.com>
2015-08-12 21:24:38 -0400

    rm redundant override of a method in Block

--- src/main/java/io/github/derpsquad/portalmod/block/BlockSpawnTeleporter.java
+++ src/main/java/io/github/derpsquad/portalmod/block/BlockSpawnTeleporter.java
@@ -9,10 +9,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.EnumWorldBlockLayer;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockSpawnTeleporter extends ModBlock {
 
@@ -25,14 +22,7 @@ public class BlockSpawnTeleporter extends ModBlock {
 	public String getName() {
 		return name;
 	}
-
-	// the block will render in the SOLID layer.  See http://greyminecraftcoder.blogspot.co.at/2014/12/block-rendering-18.html for more information.
-	@SideOnly(Side.CLIENT)
-	public EnumWorldBlockLayer getBlockLayer()
-	{
-		return EnumWorldBlockLayer.SOLID;
-	}
-
+	
 	// Called when adjacent block updates
 	@Override
     public void onNeighborBlockChange(World worldIn, BlockPos pos, IBlockState state, Block neighborBlock)
